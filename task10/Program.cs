19435e6604f1b8b0b711652873db010c51b8b6ce
VVnetten <slav133@yandex.ru>
2023-07-21 12:29:24 +0300

    add all folders

---
+++ task10/Program.cs
@@ -0,0 +1,6 @@
+﻿
+// Console.WriteLine("Введите трехзначное число:");
+// int num = int.Parse(Console.ReadLine());
+// int num3 = num / 10;
+// int num2 = num3 % 10;
+// Console.WriteLine($"{num} -> {num2}:");
---
+++ task13/Program.cs
@@ -0,0 +1,13 @@
+﻿// Console.WriteLine("Введите число:");
+// int num = int.Parse(Console.ReadLine());
+// int n = num;
+// if (num / 100 == 0 ) {
+//    Console.WriteLine("третьей цифры нет"); 
+// }
+// else {
+//     while (n/1000 >0)
+//     {
+//         n = n/10;
+//     } 
+// Console.WriteLine($"{num} -> {n%10}:");
+// }
